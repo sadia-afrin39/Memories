1432319bf6b45e41f7461ebdd221e27c62f0fdba
Sadia Afrin Tarin <tarin.cse5.bu@gmail.com>
2023-07-10 23:07:16 +0600

    signin signup form & login with google

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,22 @@
-import React,{useState, useEffect} from 'react';
-import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
-import {useDispatch} from 'react-redux';
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Container} from '@material-ui/core';
 
-import {getPosts} from './actions/posts.js';
-import memories from './images/memories.png';
-import Posts from './components/Posts/Posts.js';
-import Form from './components/Form/Form.js';
-import useStyles from './styles';
+import Navbar from './components/Navbar/Navbar'; 
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
 
 const App = () => {
-    const [currentId, setCurrentId] = useState(null);
-    const classes = useStyles();
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(getPosts());
-    },[currentId, dispatch]);
-
     return(
-       <Container maxWidth= "lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
-                <img className={classes.image} src={memories} alt="icon" height="60" />
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid container className={classes.mainContainer} justifyContent="space-between" alignItems='stretch' spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setcurrentId={setCurrentId}/>
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setcurrentId={setCurrentId}/>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-       </Container>
+        <BrowserRouter>
+            <Container maxWidth= "lg">
+                <Navbar/>
+                <Routes>
+                    <Route path='/' element={<Home/>} />
+                    <Route path='/auth' element={<Auth/>} />
+                </Routes>
+            </Container>
+       </BrowserRouter>
     ); 
 }
 export default App;
--- server/index.js
+++ server/index.js
@@ -12,6 +12,10 @@ dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({  limit:"30mb", extended: true }));
 app.use(cors());
+// app.use((req, res, next) => {
+//     res.setHeader('Cross-Origin-Opener-Policy', 'unsafe-none'); 
+//     next();
+//   });
 
 app.use('/posts',postRoutes);
 
